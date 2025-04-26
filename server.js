3892060a237ded28e87b3d6208351a13d3737560
Claudio-13101970 <civicachiavenna@musica-viva.it>
2025-04-26 16:19:43 +0200

    Fix server.js con path corretto

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path'); // <--- QUESTA parte era mancante
 
 const app = express();
 const server = http.createServer(app);
@@ -8,8 +9,10 @@ const io = new Server(server);
 
 // Serve i file statici della cartella "public"
 app.use(express.static('public'));
+
+// Servire il file HTML per la stanza riunioni
 app.get('/stanza_riunioni', (req, res) => {
-  res.sendFile(__dirname + '/public/stanza_riunioni.html');
+  res.sendFile(path.join(__dirname, 'public', 'stanza_riunioni.html'));
 });
 
 // Evento di connessione WebSocket
@@ -36,5 +39,4 @@ io.on('connection', (socket) => {
 // Avvia il server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server WebSocket in ascolto sulla porta ${PORT}`);
-});
+  console.log(`Server WebSocket in ascolto sulla porta ${
